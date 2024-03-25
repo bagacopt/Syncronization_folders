42ae9f4ba331c773257ff944a355353f89bc7063
bagaco <tomas1234gouveia@gmail.com>
2024-03-25 12:58:47 +0000

    some development of the code, configuration of timer, some verifications (just basic code, needs to be adjusted and more complex)

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.Windows.Forms;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Timers;
+using System.Threading;
 
 namespace Syncronization_folders
 {
@@ -12,6 +14,7 @@ namespace Syncronization_folders
     {
         static string ChooseSourceFolder()
         {
+            Console.WriteLine("Choose the folder you want to backup..");
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             if (fbd.ShowDialog() == DialogResult.OK)
                 return fbd.SelectedPath;
@@ -21,6 +24,7 @@ namespace Syncronization_folders
 
         static string ChooseReplicaFolder()
         {
+            Console.WriteLine("Choose a folder you want to store your backup..");
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             if (fbd.ShowDialog() == DialogResult.OK)
                 return fbd.SelectedPath;
@@ -38,14 +42,66 @@ namespace Syncronization_folders
             return Directory.GetFiles(backup);
         }
 
-        static bool VerifyBackupFolder(string[] sourcefiles, string[] backupfiles)
+        static int ConfigureTimerTime()
         {
-            if (sourcefiles.Length != backupfiles.Length)
+            Console.WriteLine("What interval of time you want the program to look for changes on the source folder? (seconds [ default: 300 seconds { 5 minutes } ] )");
+            int interval = Convert.ToInt32(Console.ReadLine());
+
+            return interval;
+        }
+
+        static bool VerifyLastModification(string source, string backup)
+        {
+
+            DateTime lastModifiedSource = Directory.GetLastWriteTime(source);
+            DateTime lastModifiedBackup = Directory.GetLastWriteTime(backup);
+
+            if (lastModifiedSource > lastModifiedBackup)
             {
-                return false;
+                return true;
             }
 
-            return true;
+            return false;
+        }
+
+        static bool VerifyFolderToSave()
+        {
+            Console.WriteLine("Are you sure this is the folder you want to create a backup of? (1 - YES; 0 - NO)");
+            int saveSource = Console.Read();
+
+            Console.WriteLine("Are you sure this is where you want to keep your backup? (1 - YES; 0 - NO)");
+            int saveBackup = Console.Read();
+
+            if (saveSource == 1 && saveBackup == 1)
+            {
+                ConfigureTimerTime();
+                return true;
+
+            }
+            else if (saveSource == 1 && saveBackup != 1)
+            {
+                ChooseReplicaFolder();
+                VerifyFolderToSave();
+                return false;
+            }
+            else if (saveSource != 1 && saveBackup == 1)
+            {
+                ChooseSourceFolder();
+                VerifyFolderToSave();
+                return false;
+            }
+            else
+            {
+                ChooseSourceFolder();
+                ChooseReplicaFolder();
+                VerifyFolderToSave();
+                return false;
+            }
+        }
+
+        static bool saveLogFile()
+        {
+            return false;
         }
 
 
@@ -53,19 +109,26 @@ namespace Syncronization_folders
         static void Main(string[] args)
         {
             // VARIABLES
+            #region
             string sourcePath, backupPath;
             string[] sourceFiles, backupFiles;
 
+            TimeSpan coulddown = TimeSpan.FromSeconds(ConfigureTimerTime());
+            System.Timers.Timer timer = new System.Timers.Timer(coulddown.TotalMilliseconds);
+
+            #endregion
+
+            timer.AutoReset = true;
+            timer.Stop();
+
             // GET SOURCE FOLDER
             #region
-            Console.WriteLine("Choose the folder you want to backup..");
             sourcePath = ChooseSourceFolder();
             Console.WriteLine(sourcePath);
             #endregion
 
             // CHOOSE BACKUP FOLDER
             #region
-            Console.WriteLine("Choose a folder you want to store your backup..");
             backupPath = ChooseReplicaFolder();
             Console.WriteLine(backupPath);
             #endregion
@@ -89,7 +152,26 @@ namespace Syncronization_folders
                 Console.WriteLine(file);
             }
             #endregion
-            
+
+
+            if (VerifyFolderToSave())
+            {
+                timer.Start();
+
+
+            }
+
+
+
+            // PRINT THERE ARE MODIFICATIONS ON THE FOLDER
+            #region
+            if (VerifyLastModification(sourcePath, backupPath))
+            {
+                Console.WriteLine("Existem modificações no ficheiro raiz");
+
+            }
+
+            #endregion
 
             Console.ReadLine();
             
